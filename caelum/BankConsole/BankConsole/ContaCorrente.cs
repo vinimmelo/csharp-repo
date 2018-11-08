958be753c8c65a5be4fb7143a7e9af8f00e80231
Melo <Vinicius.Melo1@br.nestle.com>
2018-11-07 22:46:50 -0200

    Added Console Project: Bank!

---
+++ caelum/BankConsole/BankConsole/ContaCorrente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankConsole
+{
+    class ContaCorrente
+    {
+        public double saldo;
+        public int numero;
+        public int agencia;
+        public string nome;
+    }
+}
---
+++ caelum/BankConsole/BankConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente primeiraConta = new ContaCorrente();
+            primeiraConta.saldo = 200;
+            Console.WriteLine("Primeira Conta saldo: " + primeiraConta.saldo);
+            primeiraConta.saldo += 100;
+            Console.WriteLine("Primeira Conta saldo: " + primeiraConta.saldo);
+            ContaCorrente segundaConta = new ContaCorrente();
+            segundaConta.saldo = 50;
+
+            Console.WriteLine("Segunda conta saldo:" + segundaConta.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
