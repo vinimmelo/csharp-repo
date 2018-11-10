aabd0a4f93ac4b45e7bb544bd21686f8c05ee0b1
Melo <Vinicius.Melo1@br.nestle.com>
2018-11-10 00:08:16 -0200

    Flashy Thing, change  the background color in a infinite loop!

---
+++ complete-codes/FlashyThing/FlashyThing/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace FlashyThing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            while (Visible)
+            {
+                 for (int c = 1; c < 255 && Visible; c++)
+                {
+                    this.BackColor = Color.FromArgb(c, 255 - c, c);
+                    Application.DoEvents();
+                    Thread.Sleep(5);
+                }
+
+                for (int c = 255; c > 1 && Visible; c--)
+                {
+                    this.BackColor = Color.FromArgb(c, 255 - c, c);
+                    Application.DoEvents();
+                    Thread.Sleep(5);
+                }
+            }
+            
+        }
+    }
+}
