c2724ecc5d0216d25c2e3132a60c21ed9db9b0f1
Vinícius Melo <viniciusdesk@icloud.com>
2018-11-16 00:14:02 -0200

    Added exception training project, tested NullReferenceException and DivisionByZero.

---
+++ caelum/Exceptions/ExceptionTraining/ExceptionTraining/ContaCorrente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionTraining
+{
+    class ContaCorrente
+    {
+        private string teste;
+
+        public string NullAcess { get; set; }
+    }
+}
---
+++ caelum/Exceptions/ExceptionTraining/ExceptionTraining/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionTraining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Metodo();
+            }
+            catch (NullReferenceException er)
+            {
+                Console.WriteLine(er.Message);
+                Console.WriteLine(er.StackTrace);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            
+        }
+
+        public static int Dividir(int numero, int divisor)
+        {
+            ContaCorrente cc = null;
+            cc.NullAcess = "Test";
+            return numero / divisor;
+        }
+
+        static void Metodo()
+        {
+            TestaDivisao(0);
+        }
+
+        static void TestaDivisao(int divisor)
+        {
+            Dividir(10, divisor);
+        }
+    }
+}
