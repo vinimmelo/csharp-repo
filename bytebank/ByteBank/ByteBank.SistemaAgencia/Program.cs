0e34d013c6442b2957bd39d24553ad956c4b8bde
Vinícius Melo <viniciusdesk@icloud.com>
2019-01-02 12:08:07 -0200

    Added Racetrack Simulator and improved ByteBank with two more systems.

---
+++ bytebank/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(2545, 12345678);
+            Console.WriteLine(conta.Saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
